98a75452c40c33f860003228e0a915b6fc2b8993
ParadoxPD <ask.priyanshu@gmail.com>
2022-08-11 20:41:03 +0530

    Python and ML Bootcamp Assignment 1

---
+++ Python and ML Bootcamp/Assignment 1/Assignment.py
@@ -0,0 +1,23 @@
+'''
+Topics:
+
+    What is Python?
+    Python Variables
+    Python Operators
+        Python Arithmetic Operators
+        Python Comparison Operators
+        Python Logical Operators
+        Python Identity Operators
+    Python Data Types
+        Numeric
+            Integer
+            Float
+            Complex Numbers
+        String
+        List
+        Tuple
+        Boolean
+        Dictionary
+        Set
+
+'''
