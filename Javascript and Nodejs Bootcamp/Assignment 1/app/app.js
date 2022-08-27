0f78cd5a203149d80e214e1965631fcce1139925
ParadoxPD <ask.priyanshu@gmail.com>
2022-08-27 23:03:56 +0530

    JavaScript and Nodejs Bootcamp Assignment 2(Done)

--- Javascript and Nodejs Bootcamp/Assignment 1/app/app.js
+++ Javascript and Nodejs Bootcamp/Assignment 1/app/app.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const data = require("./data/movies.json");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const data = require("./data/db.json");
 const PORT = process.env.PORT || 5555;
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  let responseText = { movies: [] };
-  data["movies"].forEach((movie) => {
-    responseText["movies"].push(movie["title"]);
-  });
+  // let responseText = { movies: [] };
+  // data["movies"].forEach((movie) => {
+  //   responseText["movies"].push(movie["title"]);
+  // });
   // console.log(responseText);
+  let responseText = data["movies"].push();
   res.send(responseText);
+});
+
+app.post("/", (req, res) => {
+  let newData = data["movies"];
+  let newMovies = req.body.movies;
+  // console.log(newMovies);
+  newMovies.forEach((movie) => {
+    newData.push(movie);
+  });
+  // console.log(newData[newData.length - 1]);
+  fs.writeFile("./data/db.json", JSON.stringify(newData), function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500);
+    }
+  });
+  res.send("done");
 });
 
 app.listen(PORT, (err) => {
